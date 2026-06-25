ad2f845afad991b08ac7b7d5a0139629901c59ec
Himanshu Singh <himanshu124singh19@gmail.com>
2026-06-25 21:27:21 +0530

    Frontend Created

--- perfAnalyzer-frontend/src/app/components/execution-controls/execution-controls.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-execution-controls',
-  imports: [],
-  templateUrl: './execution-controls.html',
-  styleUrl: './execution-controls.css',
-})
-export class ExecutionControls {}
